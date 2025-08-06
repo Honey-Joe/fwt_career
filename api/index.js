deae33e7c51554184abb87f23cbcbdfc1fe9d85f
Honey-Joe <honeyjoe943@gmail.com>
2025-08-06 22:59:55 +0530

    configured fwt_career

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv")
+
+const app = express();
+
+dotenv.config();
+
+app.get("/", async(req,res)=>{
+    res.send("test api")
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT ,()=>{
+    console.log("The Server Running on port "+PORT)
+})
